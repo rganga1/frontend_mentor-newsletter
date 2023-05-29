1bc532a7176e2fb066d5b569892b186db1188b93
RakeshG <rockingrakeshshetty@gmail.com>
2023-05-29 15:08:36 +0530

    Initial setup - vite-tailwind-plugins

---
+++ src/App2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App2() {
+  return (
+    <div className="prose">
+      <span>App2</span>
+      <h1>abc</h1>
+      <h2 className="-mt-4">def</h2>
+    </div>
+  );
+}
+
+export default App2;
---
+++ src/App4.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App4() {
+  return (
+    <div className="prose">
+      <span>App4</span>
+      <h1>abc</h1>
+      <h2 className="-mt-4">def</h2>
+    </div>
+  );
+}
+
+export default App4;
