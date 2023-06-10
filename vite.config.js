8d5071c7d99585d3c93a2782a2a317ae1b476148
Rakesh G <85186448+rganga1@users.noreply.github.com>
2023-06-10 19:23:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rganga1.github.io/frontend_mentor-newsletter/"
+  base: "/frontend_mentor-newsletter/"
 })
