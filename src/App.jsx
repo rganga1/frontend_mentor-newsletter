9af858e0c15dc198412df9a48cc2e6f18e1d4d8f
RakeshG <rockingrakeshshetty@gmail.com>
2023-06-11 15:51:16 +0530

    added homepage in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
-import Newsletter from "./pages/NewsLetter.jsx"
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import Newsletter from "./pages/NewsLetter.jsx";
 import Success from "./pages/Success.jsx";
 
 function App1() {
   return (
-    <HashRouter basename='/frontend_mentor-newsletter'>
-    <Routes>
-      <Route path="/" element={<Newsletter/>}/>
-      <Route path="/success" element={<Success/>}/>
-    </Routes>
-    </HashRouter>
+    <>
+      <h1>Navbar</h1>
+      <HashRouter basename="/frontend_mentor-newsletter">
+        <Routes>
+          <Route path="/" element={<Newsletter />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
